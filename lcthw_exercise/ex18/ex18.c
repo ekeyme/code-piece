148dcd891fac623f16e465d63c2ab589d6ab8563
Ekeyme Mo <ekeyme@gmail.com>
2017-07-19 23:42:37 +0800

    lcthw: ex18

--- lcthw_exercise/ex18/ex18.c
+++ lcthw_exercise/ex18/ex18.c
@@ -28,15 +28,15 @@ int *bubble_sort(int *numbers, int count, compare_cb cmp)
 	int temp = 0;
 	int i = 0;
 	int j = 0;
-	int *target = malloc(count*sizeof(int));
+	int *target = malloc(count * sizeof(int));
 
 	if(!target) die("Memory error.");
 
-	memcpy(target, numbers, count*sizeof(int));
+	memcpy(target, numbers, count * sizeof(int));
 
 	for(i = 0; i < count; i++) {
 		for(j = 0; j < count - 1; j++) {
-			if(cmp(target[j], target[j+1])){
+			if(cmp(target[j], target[j+1]) > 0){
 				temp = target[j+1];
 				target[j+1] = target[j];
 				target[j] = temp;
