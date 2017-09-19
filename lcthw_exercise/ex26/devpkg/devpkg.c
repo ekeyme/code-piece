89d011dd29371e7b4c7d99b715964334df214f8a
Ekeyme Mo <ekeyme@gmail.com>
2017-09-19 23:26:00 +0800

    tmp

--- lcthw_exercise/ex26/devpkg/devpkg.c
+++ lcthw_exercise/ex26/devpkg/devpkg.c
@@ -25,6 +25,81 @@ int main(int argc, char const *argv[])
 	const char *url = NULL;
 	enum CommandType request = COMMAND_NONE;
 
+	rv = apr_getopt_init(&opt, p, argc, argv);
+
+	while(apr_getopt(opt, "I:Lc:m:i:d:SF:B:", &ch, &optarg) == APR_SUCCESS) {
+		switch(ch) {
+			case 'I':
+				request = COMMAND_INSTALL;
+				url = optarg;
+				break;
+
+			case 'L':
+				request = COMMAND_LIST;
+				break;
+
+			case 'c':
+				config_opts = optarg;
+				break;
+
+			case 'm':
+				make_opts = optarg;
+				break;
+
+			case 'i':
+				install_opts = optarg;
+				break;
+
+			case 'S':
+				request = COMMAND_INIT;
+				break;
+
+			case 'F':
+				request = COMMAND_FETCH;
+				url = optarg;
+				break;
+
+			case 'B':
+				request = COMMAND_BUILD;
+				url = optarg;
+				break;
+
+			default:
+				// do nothing
+		}
+	}
+
+	switch(request) {
+		case COMMAND_INSTALL:
+			check(url, "You must at least give a URL.");
+			Command_install(p, url, config_opts, make_opts, install_opts);
+			break;
+
+		case COMMAND_LIST:
+			int rc = DB_list();
+			check(rc == 0, "Failed to list packages.");
+			break;
+
+		case COMMAND_FETCH:
+			check(url, "You must give a URL.");
+			Command_fetch(p, url, 1);
+			log_info("Downloaded to %s and in /tmp/", BUILD_DIR);
+			break;
+
+		case COMMAND_BUILD:
+			check(url, "You must at least give a URL.");
+			Command_build(p, url, config_opts, make_opts, install_opts);
+			break;
+
+		case COMMAND_INIT:
+			rv = DB_init();
+			check(rv == 0, "Failed to make the database.");
+			break;
+
+		default:
+			sentinel("Invalid command given.");
+	}
+
 	return 0;
 
 error:
