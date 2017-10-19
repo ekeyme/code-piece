898aede6e235230d38f6e302c21e2196fd5723e6
Ekeyme Mo <ekeyme@gmail.com>
2017-10-19 18:03:55 +0800

    ex28

---
+++ lcthw_exercise/ex28/c-skeleton/tests/runtests.sh
@@ -0,0 +1,16 @@
+echo "Running unit tests:"
+
+for i in tests/*_tests; do
+	if test -x $i; then
+		if $VALGRIND ./$i 2>> tests/tests.log; then
+			echo $i PASS
+		else
+			echo "ERROR in test $i: here's tests/tests.log"
+			echo "-----"
+			tail tests/tests.log
+			exit 1
+		fi
+	fi
+done
+
+echo ""
